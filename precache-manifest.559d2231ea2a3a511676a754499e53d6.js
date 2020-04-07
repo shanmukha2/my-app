2038b6faf209f489cc10d14da496551aa6e6fcde
shanmukha2 <root@ip-172-31-11-189.us-east-2.compute.internal>
2020-04-07 00:23:29 +0000

    Updates

---
+++ precache-manifest.559d2231ea2a3a511676a754499e53d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9640fa693ff3b5a5246c4740f400f38",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c4264d95558504671e70",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6210bf1ded25448980ff",
+    "url": "/my-app/static/js/2.2594f893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2594f893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4264d95558504671e70",
+    "url": "/my-app/static/js/main.cdae934f.chunk.js"
+  },
+  {
+    "revision": "293656d52bf581d13418",
+    "url": "/my-app/static/js/runtime-main.9e7aca70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
